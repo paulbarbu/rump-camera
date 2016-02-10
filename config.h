c709fffa42b38a2e5bc6caa4bd6bc1fc134a407f
Barbu Paul - Gheorghe <barbu.paul.gheorghe@gmail.com>
2016-02-10 21:34:50 +0200

    better interface and naming for the manager, also make sure free space is kept for newer videos

--- config.h
+++ config.h
@@ -7,10 +7,13 @@
 //                     videoYYYY-  MM-  DD-  hh-  mm- ss
 #define FILENAME "/tmp/video%04d-%02d-%02d-%02d-%02d-%02d.mjpeg"
 
-// there will be MAX_VIDS completed videos and one in progress
-#define MAX_VIDS 5
+// there will be MAX_VIDS-1 completed videos and one in progress
+#define MAX_VIDS 6
 
 #define FILENAME_LEN 255
+
+// maximum size (in MB) that all the videos should occupy on disk
+#define MAX_TOTAL_SIZE 100
 
 #define PAGE "<html><head><title>libmicrohttpd demo</title>"\
              "</head><body>libmicrohttpd demo %d</body></html>"
--- manager.c
+++ manager.c
@@ -13,17 +13,19 @@ struct video
 struct manager
 {
 	struct video vids[MAX_VIDS];
-	size_t num_created;
+	size_t num;
 } global_mgr;
 
 
-void video_create(struct video *v, char *filename, long int size);
-int manager_clean_old(struct manager *m);
+void _video_create(struct video *v, char *filename, long int size);
+int _manager_remove_oldest(struct manager *m);
+int _manager_clean_size(struct manager *m);
+long int _total_size(struct manager *m);
 
 
 struct manager *manager_create()
 {
-	global_mgr.num_created = 0;
+	global_mgr.num = 0;
 	return &global_mgr;
 }
 
@@ -33,58 +35,98 @@ void manager_release(struct manager *m)
 
 int manager_add(struct manager *m, char *filename, long int size)
 {		
+	int ret = 0;
+
 	//add the new video in the queue	
 	struct video v;
-	video_create(&v, filename, size);
+	_video_create(&v, filename, size);
 	
-	m->vids[m->num_created] = v;	
-	m->num_created += 1;
+	m->vids[m->num] = v;	
+	m->num += 1;
 	
-	return manager_clean_old(m);
+	if(m->num >= MAX_VIDS)
+	{
+		ret = _manager_remove_oldest(m);
+	}
+
+	ret = !ret && _manager_clean_size(m);
+
+	return ret;
 }
 
 void manager_print(struct manager *m)
 {
-	if(m->num_created < 1)
+	if(m->num < 1)
 	{
 		printf("No videos yet\n");
 		return;
 	}
 	
-	printf("From newest to oldest, %ld videos:\n", m->num_created);
+	printf("From newest to oldest, %ld videos:\n", m->num);
 		
-	for(int i=m->num_created-1; i>=0; --i)
+	for(int i=m->num-1; i>=0; --i)
 	{
 		printf("%d: %s (%ld MB)\n", i, m->vids[i].filename, m->vids[i].size);
 	}
 }
 
-int manager_clean_old(struct manager *m)
+int _manager_remove_oldest(struct manager *m)
 {
-	if(m->num_created >= MAX_VIDS)
+	if(m->num == 0)
 	{
-		m->num_created = MAX_VIDS-1;
-		
-		printf("Removing %s\n", m->vids[0].filename);
-		// delete the oldest video
-		if(-1 == unlink(m->vids[0].filename))
-		{
-			perror("unlink");
-			return 1;
-		}
-		
-		//shift the other videos to the left	
-		for(int i=1; i<MAX_VIDS; ++i)
-		{
-			m->vids[i-1] = m->vids[i];
-		}
+		printf("No items to remove\n");
+		return 0;
+	}
+
+	printf("Removing %s\n", m->vids[0].filename);
+	// delete the oldest video
+	if(-1 == unlink(m->vids[0].filename))
+	{
+		perror("unlink");
+		return 1;
+	}
+
+	//shift the other videos to the left
+	for(int i=1; i<MAX_VIDS; ++i)
+	{
+		m->vids[i-1] = m->vids[i];
 	}
 	
+	m->num -= 1;
+
 	return 0;
 }
 
-void video_create(struct video *v, char *filename, long int size)
+int _manager_clean_size(struct manager *m)
+{
+	int ret = 0;
+	long int total = _total_size(m);
+	long int average_size = total / m->num;
+
+	printf("Total size: %ld\nAverage size: %ld\n", total, average_size);
+
+	if(total + average_size > MAX_TOTAL_SIZE)
+	{
+		ret = _manager_remove_oldest(m);
+	}
+
+	return ret;
+}
+
+void _video_create(struct video *v, char *filename, long int size)
 {
 	v->size = size;
 	strncpy(v->filename, filename, FILENAME_LEN);
 }
+
+long int _total_size(struct manager *m)
+{
+	long int sum = 0;
+	for(int i=0; i < m->num; ++i)
+	{
+		sum += m->vids[i].size;
+	}
+
+	return sum;
+}
+
